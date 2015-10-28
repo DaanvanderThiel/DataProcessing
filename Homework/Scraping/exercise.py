02d900abc25bfe8ba5de713d3ade173fb1dc083a
daanownd <daanvanderthiel@gmail.com>
2015-10-28 21:45:35 +0100

    all

--- Homework/Scraping/exercise.py
+++ Homework/Scraping/exercise.py
@@ -17,13 +17,26 @@ This module contains an implementation of split_string.
 
 
 def split_string(source, separators):
-    l = []
+    resultStrings = []
+    # replace every seperator with a space
     for characters in separators:
-
-        source = source.replace(characters,"")
-    print source
-    l = list(source)
-    print l
+        source = source.replace(characters,' ')
+    source = source + " "
+    i = 0
+    string = ""
+    for char in source:
+        # add letter to string if it is no space
+        if char != ' ':
+            string += source[i]
+        else:
+            if string == '':
+                string = ""
+            else:
+                # add string to resultstrings when the next space is found
+                resultStrings.append(string)
+                string = ""
+        i = i + 1
+    return resultStrings
 '''
 Split a string <source> on any of the characters in <separators>.
 The ouput of this function should be a list of strings split at the
@@ -34,4 +47,4 @@ positions of each of the separator characters.
 if __name__ == '__main__':
     # You can try to run your implementation here, that will not affect the
     # automated tests.
-    print split_string('abacadabra', 'ba')  # should print: ['c', 'd', 'r']
+    print split_string('abc', '')  # should print: ['c', 'd', 'r']
